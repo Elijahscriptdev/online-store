51fb71d3f2309a528e57bbd1b11dac3e3dfc24f3
Elijahscriptdev <elijahobominuru@gmail.com>
2020-07-01 16:01:04 +0100

    fixed toggle button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+console.log('Hello from application.js')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -22,9 +22,18 @@ require("channels")
 // $(document).on('turbolinks:load', function () {
 
 // });
+// document.getElementById("fa-bars").addEventListener('click', () => {
+    
+//     console.log("deleteBook");
+// });
 
-
-$(document).ready(function () {
+// function myFunction() {
+//     // document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+//     console.log("yes");
+// }
+// myFunction()
+// $(document).ready(function () {
+jQuery(function ($) {
 
     $(".fa-bars").click(function () {
         $(".me").addClass("open");
